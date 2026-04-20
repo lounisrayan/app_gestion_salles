430b0a03a74514dac21d5ee083cc5fba8b143285
Rayan Lounis <lounisrayan200@gmail.com>
2026-04-19 20:51:57 -0400

    Implémentation de la connexion à la base de données

---
+++ Data/dao_salle.py
@@ -0,0 +1,18 @@
+import mysql.connector
+import json
+
+
+class DataSalle:
+
+    def get_connection(self):
+        with open("Data/config.json") as f:
+            config = json.load(f)
+
+        connection = mysql.connector.connect(
+            host=config["host"],
+            user=config["user"],
+            password=config["password"],
+            database=config["database"]
+        )
+
+        return connection
