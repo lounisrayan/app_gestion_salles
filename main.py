75e09b63603f48673fcded0d07f2a5d471cade62
Rayan Lounis <lounisrayan200@gmail.com>
2026-04-19 21:40:29 -0400

    Connexion main avec view

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-from Data.dao_salle import DataSalle
-from models.salle import Salle
+from views.view_salle import menu
 
-dao = DataSalle()
-
-# test insertion
-s = Salle("A1", "Salle Test", "Classe", 30)
-dao.insert_salle(s)
-
-# test récupération
-result = dao.get_salle("A1")
-
-if result:
-    result.afficher_infos()
-else:
-    print("Salle non trouvée")
+menu()
