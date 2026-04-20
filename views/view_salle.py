a22310d28117ab0faac8b4652b2d451ec2471117
Rayan Lounis <lounisrayan200@gmail.com>
2026-04-19 23:26:21 -0400

    Fin de l atelier application gestion des salles

--- views/view_salle.py
+++ views/view_salle.py
@@ -14,7 +14,7 @@ class ViewSalle(ctk.CTk):
 
         self.service = ServiceSalle()
 
-        # Champs
+
         self.entry_code = ctk.CTkEntry(self, placeholder_text="Code")
         self.entry_code.pack(pady=5)
 
@@ -27,7 +27,7 @@ class ViewSalle(ctk.CTk):
         self.entry_capacite = ctk.CTkEntry(self, placeholder_text="Capacité")
         self.entry_capacite.pack(pady=5)
 
-        # Boutons
+
         btn_add = ctk.CTkButton(self, text="Ajouter", command=self.ajouter_salle)
         btn_add.pack(pady=5)
 
@@ -40,7 +40,7 @@ class ViewSalle(ctk.CTk):
         btn_show = ctk.CTkButton(self, text="Afficher", command=self.charger_salles)
         btn_show.pack(pady=5)
 
-        # Tableau
+
         self.tree = ttk.Treeview(self, columns=("code", "libelle", "type", "capacite"), show="headings")
 
         self.tree.heading("code", text="Code")
@@ -50,12 +50,12 @@ class ViewSalle(ctk.CTk):
 
         self.tree.pack(pady=10)
 
-        # Charger au démarrage
+
         self.charger_salles()
 
-    # 🔥 Fonction principale (AFFICHAGE TABLEAU)
+
     def charger_salles(self):
-        # vider tableau
+
         for item in self.tree.get_children():
             self.tree.delete(item)
 
@@ -64,7 +64,7 @@ class ViewSalle(ctk.CTk):
         for s in salles:
             self.tree.insert("", "end", values=(s.code, s.libelle, s.type, s.capacite))
 
-    # Ajouter
+
     def ajouter_salle(self):
         code = self.entry_code.get()
         libelle = self.entry_libelle.get()
@@ -74,11 +74,11 @@ class ViewSalle(ctk.CTk):
         s = Salle(code, libelle, type_salle, capacite)
         self.service.ajouter_salle(s)
 
-        self.charger_salles()  # 🔥 refresh tableau
+        self.charger_salles()
 
         print("Salle ajoutée ✔")
 
-    # Modifier
+
     def modifier_salle(self):
         code = self.entry_code.get()
         libelle = self.entry_libelle.get()
@@ -88,15 +88,15 @@ class ViewSalle(ctk.CTk):
         s = Salle(code, libelle, type_salle, capacite)
         self.service.modifier_salle(s)
 
-        self.charger_salles()  # 🔥 refresh
+        self.charger_salles()
 
         print("Salle modifiée ✔")
 
-    # Supprimer
+
     def supprimer_salle(self):
         code = self.entry_code.get()
         self.service.supprimer_salle(code)
 
-        self.charger_salles()  # 🔥 refresh
+        self.charger_salles()
 
         print("Salle supprimée ✔")
